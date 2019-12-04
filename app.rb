cbd6eb288d5501710aecd4b32a83ccf62b000555
Srikalyan Megati <kalmegati@gmail.com>
2019-12-04 14:26:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end
   
   get '/name' do
-    
+    "My name is"
   end
   
   get '/hometown' do
-    
+    "My hometown is "
   end
   
   get '/favorite-song' do
-    
+    "My favorite song is "
   end
   
 end
